5ac27b8a537f46ae14994d5961ac783c872ca729
Somtochukwu Idafum <idafumsomto@gmail.com>
2025-02-19 13:07:01 -0600

    set binding datatype for the calculator view

--- LotSizeCalculator/Views/CalculatorView.xaml.cs
+++ LotSizeCalculator/Views/CalculatorView.xaml.cs
@@ -2,6 +2,8 @@
 /*
     This is the Page behind the CalculatorView.xaml. 
 */
+using LotSizeCalculator.ViewModels;
+
 namespace LotSizeCalculator.Views;
 
 public partial class CalculatorView : ContentPage
@@ -9,5 +11,6 @@ public partial class CalculatorView : ContentPage
     public CalculatorView()
     {
         InitializeComponent();
+        BindingContext = new CalculatorViewModel();
     }
 }
