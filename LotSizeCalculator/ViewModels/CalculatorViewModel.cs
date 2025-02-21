48f4ca0a2843385b07acb57b47d9d46d8d38bb2b
Somtochukwu Idafum <idafumsomto@gmail.com>
2025-02-20 23:28:57 -0600

    created currency entry and flag display

--- LotSizeCalculator/ViewModels/CalculatorViewModel.cs
+++ LotSizeCalculator/ViewModels/CalculatorViewModel.cs
@@ -14,18 +14,24 @@ public partial class CalculatorViewModel : ObservableObject
 {
 
     public ObservableCollection<Currency> Currencies { get; set; } = [];
+
+    [ObservableProperty]
+    Currency selectedCurrency;
+
     public CalculatorViewModel()
     {
         InitCurrencies();
+        // selectedCurrency = new();
+
     }
 
     public void InitCurrencies()
     {
-        Currencies.Add(new Currency { Code = "USD", Flag = "flags/usa.png" });
-        Currencies.Add(new Currency { Code = "CAD", Flag = "flags/cad.png" });
-        Currencies.Add(new Currency { Code = "GBP", Flag = "flags/gbp.png" });
-        Currencies.Add(new Currency { Code = "AUD", Flag = "flags/aud.png" });
-        Currencies.Add(new Currency { Code = "JPY", Flag = "flags/jpy.png" });
-        Currencies.Add(new Currency { Code = "CHF", Flag = "flags/chf.png" });
+        Currencies.Add(new Currency { Code = "USD", Flag = "usd.png" });
+        Currencies.Add(new Currency { Code = "CAD", Flag = "cad.png" });
+        Currencies.Add(new Currency { Code = "GBP", Flag = "gbp.png" });
+        Currencies.Add(new Currency { Code = "AUD", Flag = "aud.png" });
+        Currencies.Add(new Currency { Code = "JPY", Flag = "jpy.png" });
+        Currencies.Add(new Currency { Code = "CHF", Flag = "chf.png" });
     }
 }
