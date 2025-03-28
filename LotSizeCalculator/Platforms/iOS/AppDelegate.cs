a2d1d6177101ffd08b5946a91c28e4f1f85b8e1a
Somtochukwu Idafum <idafumsomto@gmail.com>
2025-03-27 23:59:06 -0500

    using Preference to save display intro state

--- LotSizeCalculator/Platforms/iOS/AppDelegate.cs
+++ LotSizeCalculator/Platforms/iOS/AppDelegate.cs
@@ -5,5 +5,6 @@ namespace LotSizeCalculator;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
+	//IOS Platform makes a call to get the configured maui app.
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
