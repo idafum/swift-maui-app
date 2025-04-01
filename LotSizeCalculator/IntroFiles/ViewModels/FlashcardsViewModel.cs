187b06890f17036cfce2b4d61c049c36292fb210
Somtochukwu Idafum <idafumsomto@gmail.com>
2025-04-01 13:58:47 -0500

    create the intro flash cards view model

---
+++ LotSizeCalculator/IntroFiles/ViewModels/FlashcardsViewModel.cs
@@ -0,0 +1,16 @@
+/*
+    FlashcardsViewModel
+
+    This viewmodel controls the flashcards view
+*/
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace LotSizeCalculator.IntroFiles.ViewModels;
+
+partial class FlashcardsViewModel : ObservableObject
+{
+    public FlashcardsViewModel()
+    {
+
+    }
+}
