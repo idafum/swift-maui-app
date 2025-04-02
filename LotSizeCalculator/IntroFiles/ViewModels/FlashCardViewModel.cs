beaec6b8d886b4638b2ab91f3637121f349da541
Somtochukwu Idafum <idafumsomto@gmail.com>
2025-04-02 00:07:44 -0500

    styling the flashcards

--- LotSizeCalculator/IntroFiles/ViewModels/FlashCardViewModel.cs
+++ LotSizeCalculator/IntroFiles/ViewModels/FlashCardViewModel.cs
@@ -25,6 +25,8 @@ partial class FlashCardViewModel : ObservableObject
     private void Populator()
     {
         FlashCards.Add(new FlashCard("risk_dots.png", "You don't need to trade everything!", "Start with EUR/USD, GBP/USD, or AUD/USD – master just one"));
-
+        FlashCards.Add(new FlashCard("risk_alert.png", "Risk is everything!", "Risk kills more trades than bad calls"));
+        FlashCards.Add(new FlashCard("risk_calculator.png", "Know your lot size before every trade!", "Sizing is the difference between surviving and blowing up"));
+        FlashCards.Add(new FlashCard("risk_calculator_logo.png", "Risk2 helps you trade smarter!", "A simple Lot Size Calculator"));
     }
 }
